e20ebd1d5e2bf0a62916895b1db6ddf4343d6a30
Ahmet Yıldız <ahmetyildiztr10@outlook.com>
2021-02-27 23:19:56 +0300

    Linq codes updated and new methods added.

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Channels;
 
@@ -24,7 +25,7 @@ namespace LinqProject
                     QuantityPerUnit = "16 Gb Ram", UnitPrice = 8000, UnitsInStock = 3},
 
                 new Product{ProductId = 3, CategoryId = 1, ProductName = "Hp Laptop", 
-                    QuantityPerUnit = "8 Gb Ram", UnitPrice = 6000, UnitsInStock = 2},
+                    QuantityPerUnit = "8 Gb Ram", UnitPrice = 10000, UnitsInStock = 2},
 
                 new Product{ProductId = 4, CategoryId = 2, ProductName = "Samsung Telefon", 
                     QuantityPerUnit = "4 Gb Ram", UnitPrice = 5000, UnitsInStock = 15},
@@ -33,11 +34,84 @@ namespace LinqProject
                     QuantityPerUnit = "4 Gb Ram", UnitPrice = 8000, UnitsInStock = 0}
             };
 
+            //Test(products);
+
+            //GetProducts(products);
+
+            //AnyTest(products);
+
+            //Lambda
+
+            //FindTest(products);
+            //FindAllTest(products);
+
+            //AscDescTest(products);
+            
+            //ClassicLinqTest(products);
+
+            var result = from p in products
+                join c in categories on p.CategoryId equals c.CategoryId where p.UnitPrice > 5000
+                orderby p.UnitPrice descending
+                select new ProductDto { ProductId = p.ProductId,  CategoryName = c.CategoryName, ProductName = p.ProductName ,UnitPrice = p.UnitPrice};
+
+            foreach (var productDto in result)
+            {
+                Console.WriteLine("{0} ------ {1}", productDto.ProductName, productDto.CategoryName);
+            }
+
+
+        }
+
+        private static void ClassicLinqTest(List<Product> products)
+        {
+            var result = from p in products
+                where p.UnitPrice > 6000
+                orderby p.UnitPrice descending, p.ProductName ascending
+                select new ProductDto {ProductId = p.ProductId, ProductName = p.ProductName, UnitPrice = p.UnitPrice};
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void AscDescTest(List<Product> products)
+        {
+            //Single Line Query
+
+            var result = products.Where(p => p.ProductName.Contains("top")).OrderByDescending
+                (p => p.UnitPrice).ThenByDescending(p => p.ProductName);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void FindAllTest(List<Product> products)
+        {
+            var result = products.FindAll(p => p.ProductName.Contains("top"));
+            Console.WriteLine(result);
+        }
+
+        private static void FindTest(List<Product> products)
+        {
+            var result = products.Find(p => p.ProductId == 3);
+            Console.WriteLine(result.ProductName);
+        }
+
+        private static void AnyTest(List<Product> products)
+        {
+            var result = products.Any(p => p.ProductName == "Acer Laptop");
+
+            Console.WriteLine(result);
+        }
+
+        private static void Test(List<Product> products)
+        {
             Console.WriteLine("Algoritmik----------------");
 
             foreach (var product in products)
             {
-                if (product.UnitPrice > 5000 && product.UnitsInStock >3 )
+                if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
                 {
                     Console.WriteLine(product.ProductName);
                 }
@@ -50,12 +124,8 @@ namespace LinqProject
 
             foreach (var product in result)
             {
-
                 Console.WriteLine(product.ProductName);
             }
-
-            GetProducts(products);
-
         }
 
         static List<Product> GetProducts(List<Product> products)
@@ -76,9 +146,18 @@ namespace LinqProject
 
         static List<Product> GetProductsLinq(List<Product> products)
         {
-            return products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3).ToList();
+            return products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock >= 0).ToList();
+            
+            
         }
 
+        class ProductDto
+        {
+            public int ProductId { get; set; }
+            public string CategoryName { get; set; }
+            public string ProductName { get; set; }
+            public decimal UnitPrice { get; set; }
+        }
    
         
     }
